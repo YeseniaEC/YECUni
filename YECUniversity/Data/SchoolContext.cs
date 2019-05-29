581171ff4c58b428f9372b0d6501353cfdd86a8b
Yesenia Cardenas <yesenia.cardenas@mic-software.net>
2019-05-29 12:17:59 -0700

    basic layout complete

---
+++ YECUniversity/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace YECUniversity.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<YECUniversity.Models.Student> Student { get; set; }
+    }
+}
